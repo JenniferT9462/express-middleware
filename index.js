a90243ee1bdb6df30645d8a32f863df174270320
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2024-12-05 22:02:39 -0600

    Custom middleware and express-rate-limit

---
+++ index.js
@@ -0,0 +1,44 @@
+import express from 'express';
+const app = express();
+//import third-party middleware
+import { rateLimit } from 'express-rate-limit';
+
+//JSON Middleware - to parse JSON responses
+app.use(express.json());
+
+//Custom Middleware that logs incoming request
+app.use((req, res, next) => {
+    req.time = new Date(Date.now()).toString(); //Current time
+    console.log(req.method, req.hostname, req.path, req.time); //Output: GET localhost / Thu Dec 05 2024 21:24:24 GMT-0600 (Central Standard Time) 'method hostname path time'
+    next();
+});
+
+//express-rate-limit middleware
+const limiter = rateLimit({
+    windowMs: 1 * 60 * 1000, //Per minute
+    max: 3, //Max 3 requests per 1 minute
+    message: {
+        status: 429,
+        error: "Too many, requests, please try again later."
+    }, //Displays JSON when reached limit
+    standardHeaders: 'draft-7', //This enables 'rateLimit' in the headers, 'draft-7' is the latest.
+    legacyHeaders: false, //It is standard now to make false, also to do with headers.
+
+})
+
+//Use rateLimit - before handlers
+app.use(limiter);
+
+app.get('/', (req, res) => {
+    res.send("Welcome to Express Middleware!")
+})
+
+// app.get('/rateLimit', (req, res) => {
+//     res.send("Express Rate Limit");
+// })
+
+
+ //Start the server
+ app.listen(3000, () => {
+    console.log("Example app listening on port 3000")
+})
